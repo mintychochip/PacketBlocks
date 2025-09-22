73527c0281c2f3d12817d11b05499125e82bb6ee
justi <jlo2@csub.edu>
2025-09-22 16:54:45 -0700

    cleaning script

--- packetblocks-api/build.gradle.kts
+++ packetblocks-api/build.gradle.kts
@@ -9,9 +9,6 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
 }
 
-group = providers.gradleProperty("group")
-version = providers.gradleProperty("version")
-
 java {
     withSourcesJar()
     withJavadocJar()
